7dfe0fc7b40007dbc8274d030b282d1f8010917f
Zekko <jeroendelcour@gmail.com>
2014-08-07 00:26:00 +0200

    Added logo and favicon

---
+++ testinggrounds.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<title>Testing grounds</title>
+	<meta charset="utf-8">
+	<style>
+		#one {
+			position: absolute;
+			left: 0;
+			width: 500px;
+			text-align: right;
+			background-color: blue;
+			height: 100px;
+			z-index: 2;
+		}
+		#two {
+			position: absolute;
+			left: 50px;
+			top: 50px;
+			width: 50px;
+			height: 500px;
+			background-color: green;
+			z-index: 1;
+		}
+	</style>
+</head>
+<body>
+
+<div id="one">Hello world.</div>
+
+</body>
+</html>

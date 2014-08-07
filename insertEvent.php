a6a0f63f2f95c0f216f5a69771570d6e0dcdbbff
Zekko <jeroendelcour@gmail.com>
2014-08-07 20:37:20 +0200

    Removed old commented-out mysql_query section

--- insertEvent.php
+++ insertEvent.php
@@ -1,22 +1,4 @@
 <?php
-/*
-$con=mysqli_connect("localhost:3306","root","","yearclock");
-if (mysqli_connect_errno()) {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-};
-
-$sql="INSERT INTO events (name, start, end) VALUES ('$_POST[name]','$_POST[start]','$_POST[end]')";
-
-$result=mysql_query($sql);
-
-if (!mysqli_query($con,$sql)) {
-	die('Error: ' . mysqli_error($con));
-};
-echo "1 event added";
-mysqli_close($con);
-*/
-
-// now let's try that the PDO way
 try {
 	$conn = new PDO('mysql:host=localhost;dbname=yearclock', 'root', '');
 

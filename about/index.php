8a8e8ec812cbc01d22deb0fbb5378e2487372c2b
Zekko <jeroendelcour@gmail.com>
2014-08-12 21:17:27 +0200

    Slightly altered 'about' text and updated To-Do list.

---
+++ about/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<title>YearCircler - Intuitive calendar and year planner</title>
+	<meta charset="utf-8">
+	<meta name="author" content="Jeroen Delcour">
+	<meta name="description" content="An intuitive way to organize your year.">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="../images/favicon.ico">
+	<link href='../header.css' rel='stylesheet' type='text/css'>
+	<link href='about.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="header">
+	<div class="headerLeft">
+		<a href="http://192.168.1.120/year-clock/" class="headerButton">HOME</a>
+	</div>
+	<div class="headerRight">
+		<a href="http://192.168.1.120/year-clock/about/" class="headerButton">ABOUT</a>
+	</div>
+</div>
+
+
+	<div id="wrapper">
+		<div class="logoBig" alt="Year Circler - year planner"></div>
+		
+		<h2><span>THE ANNUAL ORGANIZER</span></h1>
+		
+		<div class="description">
+<span>Have you ever had that moment in which you suddenly thought 'Wow, that week went by so fast!' or 'I can't believe it's already September!'?
+<br/>There is often so much going on in our lives we struggle to keep track. We do our best, keeping daily diaries or using weekly organizers, but it's hard to look ahead more than a week or so and we always end up losing track of the grand scheme of things. 
+<br/>At the same time, we seem to be constantly working towards the future. We set up our daily lifes to work towards building a better future for ourselves and others. While this is a noble thing, it has the unfortunate side-effect of losing track of the moment. It becomes easy to forget life as it happens around you. 
+<br/>Both of these things can be very hard. What's even harder is having to do both at once, on a daily basis. It's difficult to both 'prepare for the future' and 'seize the day'. What we need is a way to keep track of things that are far-off but also remind us of the time given to us each day. We need a different scale to view time with.
+<br/>We need something to help us slow down and take a step back. 
+<br/>Enter YearCircler. It's an annual clock, meaning it takes an entire year to complete just one full rotation. It tries to provide a way to keep track of the 'now' while seasons pass. Every day, you'll see 'today' move just a little bit, reminding you of the ever-moving present. However, it also allows you to set markers for important events in the future so you don't lose track of what's important in the long-term. These can be all kinds of things: future goals you set for yourself, important deadlines for a project you're working on (or planning to work on in the future!) or simple things like your friends' birthdays (and your own, of course!). In short: YearCircler helps you look at the future while keeping you in the present.
+<br/>
+<br/>This website is also an experiment in web design. If you have any questions or feature requests (though I can't promise anything!) please do not hesitate to send an e-mail to <a href="mailto:contact@yearcircler.com">contact@yearcircler.com</a>.</span></span>
+		</div>
+	</div>
+
+
+</body>
+</html>
